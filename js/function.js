851a279c36affd1ce7e3057b4509edf01252044b
ht7200 <251741906@qq.com>
2016-04-21 16:44:31 +0800

    contact finish

---
+++ js/function.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+
+
+	
+
+
+
+
+
+            $("#callSun").click(function() {
+                window.open('http://wpa.qq.com/msgrd?v=3&uin=1379558294&site=qq&menu=yes');
+            })
+
+
+            $("#callTing").click(function() {
+                window.open('http://wpa.qq.com/msgrd?v=3&uin=976994550&site=qq&menu=yes');
+            })
+
+
+
+
+
+
+
+
+
+        })
